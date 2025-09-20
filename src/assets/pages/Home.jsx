93f51171b86cdee01c86566a4120fe629f1e5849
sarusagar <sagarsaru030@gmail.com>
2025-09-21 00:16:37 +0545

    first commit

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import Card from "../components/Card"
+import Navbar from "../components/Navbar"
+import axios from "axios"
+
+function Home(){
+    const [blogs,setBlogs]=useState([])
+    const fetchBlogs =async()=>{
+       const response =await axios.get("http://localhost:3000/blog")
+       setBlogs(response.data.data)
+    }
+
+    useEffect(()=>{
+        fetchBlogs()
+    },[])
+    console.log(blogs)
+    return(
+        <>
+        <Navbar/>
+        <div className=" flex flex-wrap">
+            {
+                blogs.map(function(blog){
+                    return(
+                     <Card blog={blog}/>
+                    )
+
+                })
+            }
+            
+        </div>
+        </>
+        
+    )
+}
+export default Home
---
+++ src/assets/pages/SingleBlog.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react"
+import Navbar from "../components/Navbar"
+import axios from "axios"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+
+function SingleBlog(){
+     const  {id} = useParams()
+     const navigate =useNavigate()
+     const [blog,setBlog]=useState({})
+     console.log(id)
+
+    const fetchSingleBlog=async()=>{
+        const response = await axios.get("http://localhost:3000/blog/" + id)
+            setBlog(response.data.data)
+    }
+    useEffect(()=>{
+        fetchSingleBlog()
+    },[])
+
+    const deleteThisPage = async()=>{
+        const response= await axios.delete("http://localhost:3000/blog/" + id)
+        console.log(response)
+        if(response.status ===200){
+            alert("Page deleted successfully.")
+           navigate("/")
+        }else{
+            alert("Something went wrong.")
+        }
+    }
+    return(
+        <>
+        <Navbar/>
+        <div class="bg-gray-100 dark:bg-gray-800 py-8">
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex flex-col md:flex-row -mx-4">
+            <div class="md:flex-1 px-4">
+                <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+                    <img class="w-full h-full object-cover" src={'http://localhost:3000/' + blog.image} alt="Product Image"/>
+                </div>
+                <div class="flex -mx-2 mb-4">
+                    <div class="w-1/2 px-2">
+                        <button class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Me</button>
+                    </div>
+                    <div class="w-1/2 px-2">
+                        <button class="w-full bg-gray-200 dark:bg-gray-700 text-red-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteThisPage}>Delete ME</button>
+                    </div>
+                </div>
+            </div>
+            <div class="md:flex-1 px-4">
+                <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog.title}</h2>
+                <p class="text-gray-600 dark:text-gray-300 text-sm mb-4">
+                    {blog.subtitle}
+                </p>
+                <div class="flex mb-4">
+                    <div class="mr-4">
+                        <span class="font-bold text-gray-700 dark:text-gray-300">{blog.description}</span>
+                        <span class="text-gray-600 dark:text-gray-300">$29.99</span>
+                    </div>
+                    <div>
+                        <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
+                        <span class="text-gray-600 dark:text-gray-300">In Stock</span>
+                    </div>
+                </div>
+                
+                
+                <div>
+                    <span class="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
+                    <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                        sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
+                        lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
+                        ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
+                        sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+        </>
+    )
+}
+export default SingleBlog
